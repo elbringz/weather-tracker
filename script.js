8cd6c6d9bda65c5cf02b0d6140c752c34f99ec97
Zachary Elbring <elbringzach@gmail.com>
2023-11-01 13:22:33 -0500

    Added base html and linked css and javascript to html page.

---
+++ script.js
@@ -0,0 +1,21 @@
+var searchBtn = document.querySelector('#btn');
+var searchContent = document.querySelector('#srch');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+searchBtn.addEventListener('click', function {
+    fetch
+})
