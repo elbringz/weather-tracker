e6c31f24eb803ab43f4be7271759e0268ccf1c01
Zachary Elbring <elbringzach@gmail.com>
2023-11-05 11:22:48 -0600

    Added api link with key to javascript and created a function.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var searchBtn = document.querySelector('#btn');
 var searchContent = document.querySelector('#srch');
 
+function getData() {
+
+}
 
 
 
-
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+searchContent.value+'&appid=b49714e05c2fbb6dd1d6371b8f1f9816')
 
 
 
