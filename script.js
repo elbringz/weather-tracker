52ba6fbe2b3929685ece926194291201365f5f35
Zachary Elbring <elbringzach@gmail.com>
2023-11-06 15:21:50 -0600

    Reworked both functions and made humidity temp and wind speed display from search.

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 var searchBtn = document.querySelector('#btn');
 var searchContent = document.querySelector('#srch');
+var apiKey = 'b49714e05c2fbb6dd1d6371b8f1f9816';
 
-function getData() {
+function renderWeather(weather) {
+console.log(weather);
+var results = document.querySelector('#weather1');
+var location = document.createElement('h2');
+location.textContent = weather.city.name + ' ' + weather.list[0].dt_txt;
+results.append(location);
+var temp = document.createElement('p');
+temp.textContent = 'Temp: '+weather.list[0].main.temp+' degrees';
+results.append(temp);
 
+var wind = document.createElement('p');
+wind.textContent = 'Wind: ' + weather.list[0].wind.speed + ' mph';
+results.append(wind);
+var humidity = document.createElement('p');
+humidity.textContent = 'Humidity: ' + weather.list[0].main.humidity + '%';
+results.append(humidity);
+// p for temp, wind, humidity
+}
+
+function fetchResults(query) {
+    var url = 'https://api.openweathermap.org/data/2.5/forecast?q='+searchContent.value+'&units=imperial&appid='+apiKey;
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderWeather(data));
+    
+    
 }
 
 
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+searchContent.value+'&appid=b49714e05c2fbb6dd1d6371b8f1f9816'
-.then(response => response.json())
-
-)
 
 
 
@@ -22,6 +44,3 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+searchContent.value+
 
 
 
-searchBtn.addEventListener('click', function {
-    fetch
-})
