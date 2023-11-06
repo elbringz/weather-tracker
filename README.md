2fb0b905b97088cc63d3b4ce750c9fc32d29b299
Zachary Elbring <elbringzach@gmail.com>
2023-11-06 17:44:18 -0600

    Added comments, moved js and css to assets folder.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # weather-tracker
-A webpage to check the weather.
+
+## Description
+A webpage to check the weather. This project helped me understand how API's can be used in javascript.
+
+## Screenshot
+![Screenshot of webpage](image-1.png)
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
+//Set variables
 var searchBtn = document.querySelector('#btn');
 var searchContent = document.querySelector('#srch');
 var apiKey = 'b49714e05c2fbb6dd1d6371b8f1f9816';
 
+//Function that sets weather content on page
 function renderWeather(weather) {
 console.log(weather);
+
+// Weather for day 1
 var results = document.querySelector('#weather1');
 var location = document.createElement('h2');
 location.textContent = weather.city.name + ' ' + weather.list[0].dt_txt;
@@ -11,14 +15,14 @@ results.append(location);
 var temp = document.createElement('p');
 temp.textContent = 'Temp: '+weather.list[0].main.temp+' degrees';
 results.append(temp);
-
 var wind = document.createElement('p');
 wind.textContent = 'Wind: ' + weather.list[0].wind.speed + ' mph';
 results.append(wind);
 var humidity = document.createElement('p');
 humidity.textContent = 'Humidity: ' + weather.list[0].main.humidity + '%';
 results.append(humidity);
-// p for temp, wind, humidity
+
+// Weather for day 2
 var resultsDay2 = document.querySelector('#weather2');
 var location2 = document.createElement('h2');
 location2.textContent = weather.city.name + ' ' + weather.list[8].dt_txt;
@@ -26,7 +30,6 @@ resultsDay2.append(location2);
 var tempDay2 = document.createElement('p');
 tempDay2.textContent = 'Temp: '+weather.list[8].main.temp+' degrees';
 resultsDay2.append(tempDay2);
-
 var windDay2 = document.createElement('p');
 windDay2.textContent = 'Wind: ' + weather.list[8].wind.speed + ' mph';
 resultsDay2.append(windDay2);
@@ -34,8 +37,7 @@ var humidityDay2 = document.createElement('p');
 humidityDay2.textContent = 'Humidity: ' + weather.list[8].main.humidity + '%';
 resultsDay2.append(humidityDay2);
 
-
-
+//Weather for day 3
 var resultsDay3 = document.querySelector('#weather3');
 var location3 = document.createElement('h2');
 location3.textContent = weather.city.name + ' ' + weather.list[16].dt_txt;
@@ -43,7 +45,6 @@ resultsDay3.append(location3);
 var tempDay3 = document.createElement('p');
 tempDay3.textContent = 'Temp: '+weather.list[16].main.temp+' degrees';
 resultsDay3.append(tempDay3);
-
 var windDay3 = document.createElement('p');
 windDay3.textContent = 'Wind: ' + weather.list[16].wind.speed + ' mph';
 resultsDay3.append(windDay3);
@@ -51,7 +52,7 @@ var humidityDay3 = document.createElement('p');
 humidityDay3.textContent = 'Humidity: ' + weather.list[16].main.humidity + '%';
 resultsDay3.append(humidityDay3);
 
-
+//Weather for day 4
 var resultsDay4 = document.querySelector('#weather4');
 var location4 = document.createElement('h2');
 location4.textContent = weather.city.name + ' ' + weather.list[24].dt_txt;
@@ -59,7 +60,6 @@ resultsDay4.append(location4);
 var tempDay4 = document.createElement('p');
 tempDay4.textContent = 'Temp: '+weather.list[24].main.temp+' degrees';
 resultsDay4.append(tempDay4);
-
 var windDay4 = document.createElement('p');
 windDay4.textContent = 'Wind: ' + weather.list[24].wind.speed + ' mph';
 resultsDay4.append(windDay4);
@@ -67,7 +67,7 @@ var humidityDay4 = document.createElement('p');
 humidityDay4.textContent = 'Humidity: ' + weather.list[24].main.humidity + '%';
 resultsDay4.append(humidityDay4);
 
-
+//Weather for day 5
 var resultsDay5 = document.querySelector('#weather5');
 var location5 = document.createElement('h2');
 location5.textContent = weather.city.name + ' ' + weather.list[32].dt_txt;
@@ -75,7 +75,6 @@ resultsDay5.append(location5);
 var tempDay5 = document.createElement('p');
 tempDay5.textContent = 'Temp: '+weather.list[32].main.temp+' degrees';
 resultsDay5.append(tempDay5);
-
 var windDay5 = document.createElement('p');
 windDay5.textContent = 'Wind: ' + weather.list[32].wind.speed + ' mph';
 resultsDay5.append(windDay5);
@@ -85,6 +84,8 @@ resultsDay5.append(humidityDay5);
 return;
 }
 
+
+//Function that fetches content from api and saves previous search to local storage
 function fetchResults(query) {
     var url = 'https://api.openweathermap.org/data/2.5/forecast?q='+searchContent.value+'&units=imperial&appid='+apiKey;
 
